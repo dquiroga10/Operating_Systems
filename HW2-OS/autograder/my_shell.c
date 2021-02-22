90494b362dc8bd241248e0c1c0c920d1b4962781
Daniel Quiroga <dquiroga@email.wm.edu>
2021-02-22 07:19:42 -0500

    Homework 2 finished

--- HW2-OS/autograder/my_shell.c
+++ HW2-OS/autograder/my_shell.c
@@ -1,16 +1,36 @@
-#include  <stdio.h>
-#include  <sys/types.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include  <sys/wait.h>
+#include <sys/wait.h>
 #include <errno.h>
 #include "list.h"
+#include <signal.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
 extern int errno ;
+
+
+
+//Daniel Quiroga
+//February 22, 2021
+//Homework 2 - Operating Systems
+
+struct linked_list* list;
+int sig;
+
+//This is our signal catcher for when Ctrl+C is clicked
+void controlC(int sign_num){
+	signal(SIGINT, controlC);
+	sig = 1;
+	//change our global sig variable and kill any process currently in the foreground
+	if(ll_length(list)>0){
+		kill(ll_remove_first(list), SIGKILL);
+	}
+}
 /* Splits the string by space and returns the array of tokens
 *
 */
@@ -46,6 +66,7 @@ int main(int argc, char* argv[]) {
 	char  line[MAX_INPUT_SIZE];            
 	char  **tokens;              
 	int i;
+	signal(SIGINT,controlC);
 	
 	//char delim[] = " ";
 
@@ -59,10 +80,12 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	struct linked_list* list = ll_create();
+	//here we create our list that will keep track of every PID
+	list = ll_create();
 
 	while(1) {			
 		/* BEGIN: TAKING INPUT */
+		sig= 0;
 		bzero(line, sizeof(line));
 		if(argc == 2) { // batch mode
 			if(fgets(line, sizeof(line), fp) == NULL) { // file reading finished
@@ -79,37 +102,40 @@ int main(int argc, char* argv[]) {
 
 		line[strlen(line)] = '\n'; //terminate with new line
 		tokens = tokenize(line);
+
+		//these flag variable will tell the program which condition the current commands fall under 
 		int length = 0;
 		int dAnd = 0;
-		int tAnd = 0;
 		int multiple = 0;
 		int exit = 0;
+
+		//checking to see if we have an exit command or multiple commands separate by double or triple &
 		for(i=0;tokens[i]!=NULL;i++){
 			length = i;
 			if ( (strcmp(tokens[i], "&&") == 0)){
 				dAnd =1;
 				multiple =1;
 			} else if(strcmp(tokens[i], "&&&") == 0){
-				tAnd = 1;
 				multiple =1;
 			} else if(strcmp(tokens[i], "exit") == 0){
 				exit = 1;
 			}
 		}
 
+		//initializing our array for command and path variable to call executables
 		char *args[64];
 		int status;
 		char path[] = "/bin/";
 		pid_t pid;
 		
-
-		//TODO: make it wait in the foreground while parallel commands finish
+		//two separate section one for multiple commands and the other for single commands
 		if (multiple == 1){
 			char *Margs[64][64];
 			int Length[64];
 			int cc = 0;
 			int ci = 0;
 			int curr_length = ll_length(list);
+			//we place all commands in its own index of Margs such that the first index refers to command number and the second is the token in that command
 			for(i=0;tokens[i]!=NULL;i++){
 				if ( (strcmp(tokens[i], "&&") == 0) || (strcmp(tokens[i], "&&&") == 0)){
 					Length[cc] = ci;
@@ -124,56 +150,69 @@ int main(int argc, char* argv[]) {
 				}
 			}
 			Length[cc] = ci;
-			int pids[64];
+
+			//here we iterate through each command that was inputted
 			for (i=0;i <= cc;i++){
-			 	int exec;
-			 	int execution;
-			 	char *argv[64];
-			 	for(int j = 0; j<Length[i];j++) {
-			 		if (j==0){
-			 			strcpy(path, "/bin/");
-			 			strcat(path, Margs[i][j]);
-			 			argv[j] = path;
-			 		}
-			 		else{
-			 			argv[j] = Margs[i][j];	
-			 		}
-			 	}
-			 	pid = fork();
-			 	if(pid>0){
-			 		ll_add(list, pid);
-			 	}
-			 	if(pid==0){
-			 		if (strcmp(argv[0],"/bin/cd") == 0){
-						
-						exec = chdir(argv[1]);
+				printf("sig: %d\n",sig );
+				if(sig == 0){
+				 	int exec;
+				 	int execution;
+				 	char *argv[64];
+				 	//we go through each command in Margs and place in an array that will be easier for execution lower
+				 	for(int j = 0; j<Length[i];j++) {
+				 		if (j==0){
+				 			strcpy(path, "/bin/");
+				 			strcat(path, Margs[i][j]);
+				 			argv[j] = path;
+				 		}
+				 		else{
+				 			argv[j] = Margs[i][j];	
+				 		}
+				 	}
 
-					}else if (strcmp(argv[0],"/bin/exit") == 0){
-						exit = 1;
-					}
-					else{
-						exec = execv(argv[0], argv);
-					}
-			 	}
-			 	else{
-			 		pids[i] = pid;
-			 		if(dAnd == 1){
-			 			waitpid(pid, &execution, 0);
-			 			ll_remove_first(list);
-			 		}
-			 	}
+				 	//create child process and add that pid to the list of pids currently active
+				 	pid = fork();
+				 	if(pid>0){
+				 		ll_add(list, pid);
+				 	}
+
+				 	//here we will execute each command and depending on the
+				 	//flag we will do serial or parallel
+				 	if(pid==0){
+				 		if (strcmp(argv[0],"/bin/cd") == 0){
+							
+							exec = chdir(argv[1]);
+
+						}else if (strcmp(argv[0],"/bin/exit") == 0){
+							exit = 1;
+						}
+						else{
+							exec = execv(argv[0], argv);
+						}
+				 	}
+				 	else{
+				 		if(dAnd == 1){
+				 			waitpid(pid, &execution, 0);
+				 			if(ll_length(list)>0){
+				 				ll_remove_first(list);
+				 			}		
+				 		}
+				 	}
 
 
-			 	if(exec == -1){
-			 		perror("Shell: Incorrect command");
-			 	}
+				 	if(exec == -1){
+				 		perror("Shell: Incorrect command");
+				 	}
 
-			 	for(int j = 0; j<64;j++) {
-			 		argv[j] = NULL;
-			 	}
+				 	//here we free up the space used to run the command so tha the next iterating as a null terminating array
+				 	for(int j = 0; j<64;j++) {
+				 		argv[j] = NULL;
+				 	}
+				}
 			}
 
 			//wait here for pids -- parallel
+			//we will not ask for user input until all parallel processes are finished
 			int execution;
 			while(ll_length(list)>curr_length){
 				waitpid(ll_remove_first(list), &execution, 0);
@@ -181,16 +220,10 @@ int main(int argc, char* argv[]) {
 			while(ll_length(list)>0){
 				printf("Shell: Background process finished\n");
 				waitpid(ll_remove_first(list), &execution, 0);
-			}
+			}			
 
-			
-			// for (i=0;i <= cc;i++){
-			// 	printf("%d\n", pids[i]);
-			// 	waitpid(pids[i], &execution, 0);
-			// }
 
-			
-
+			//free up the space taken up by Margs and convert it back to NULL
 			for (i=0;i <= cc;i++){
 				for (ci=0; ci < Length[i];ci++){
 					Margs[i][ci] = NULL;
@@ -199,26 +232,29 @@ int main(int argc, char* argv[]) {
 		}
 		else{
 
-			//TODO: Kill all proceses that may be running and create exit command
+			//check to see if we have an background processes taht finished or need to finish and go ahead and finish them
 			int execution;
 			while(ll_length(list)>0){
 				waitpid(ll_remove_first(list), &execution, 0);
 				printf("Shell: Background process finished\n");
 			}
 			
+			//checking to see if we have a background command
 			int and = 0;
 			if(strcmp(tokens[length], "&") == 0){
 				tokens[length] = NULL;
 				and = 1;
 			}
 
+
+			//create new child process and add it to list of pids
 			pid = fork();
 			if(pid>0){
 		 		ll_add(list, pid);
 		 	}
 
 			if (pid == 0){
-				//do whatever you want with the commands, here we just print them
+				//for the command we place in a NULL terminated array in order to execute
 				for(i=0;tokens[i]!=NULL;i++){
 
 					if(i == 0){
@@ -230,6 +266,8 @@ int main(int argc, char* argv[]) {
 					}
 				}
 				int exec;
+
+				//special case for cd and exit since these are not built in commands
 				if (strcmp(args[0],"/bin/cd") == 0){
 					exec = chdir(tokens[1]);
 					
@@ -249,15 +287,14 @@ int main(int argc, char* argv[]) {
 			}
 			else{
 				if (and == 0){
-					ll_remove_first(list);
 					wait( &status );
+					ll_remove_first(list);
 				}
 				
 			}
 
 			
 		}
-  		
 		// Freeing the allocated memory	
 		for(i=0;tokens[i]!=NULL;i++){
 			free(tokens[i]);
@@ -265,15 +302,18 @@ int main(int argc, char* argv[]) {
 		}
 		free(tokens);
 
+		//we will break here if exit is seen in the tokens
 		if (exit == 1){
 			break;
 		}
 	}
 	int execution;
+	//go and kill any process that is still in the list of pids to free up memory
 	while(ll_length(list)>0){
 		printf("Shell: Kill process finished\n");
 		kill(ll_remove_first(list), SIGKILL);
 	}
+	//destroy the list used for pids
 	ll_destroy(list);
 
 	return 0;
